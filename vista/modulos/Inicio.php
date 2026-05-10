f571008003037aff0724e09450359f9c56fdd0c0
Diego336s <sanabriadiego336@gmail.com>
2026-05-09 21:42:04 -0500

    Corregir rutas de imágenes en el carrusel para unificar el formato de separación

--- vista/modulos/Inicio.php
+++ vista/modulos/Inicio.php
@@ -30,10 +30,10 @@
 
         <div class="carousel">
             <div class="carousel-slide active">
-                <img src="vista/IMG/Proyecto 1.png" alt="">
+                <img src="./vista/IMG/Proyecto 1.png" alt="">
             </div>
             <div class="carousel-slide">
-                <img src="vista/IMG/Proyecto 2.png" alt="">
+                <img src="./vista/IMG/Proyecto 2.png" alt="">
             </div>
             <div class="carousel-slide">
                 <img src="vista/IMG/Proyecto 3.png" alt="">
